d4925d2c996cec6738fd1ae4b33318434d95a55d
DMHCAIT <santoshapplication@dmhca.in>
2025-11-26 00:30:04 +0530

    Fix missing Link import in EditorialScrollStory component

--- components/store/EditorialScrollStory.tsx
+++ components/store/EditorialScrollStory.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useRef, useState } from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 import { Product } from '@/types'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
